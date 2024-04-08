9fdec2a026f6684b56176a02b32d2051b81e2d19
Tom Roar Furunes <tom.roar@halloverden.no>
2024-04-08 13:39:51 +0200

    ADD: transformHeaderKey

--- src/Helper/TransformRequestDataHelper.php
+++ src/Helper/TransformRequestDataHelper.php
@@ -3,6 +3,9 @@
 namespace HalloVerden\RequestMappingBundle\Helper;
 
 final readonly class TransformRequestDataHelper {
+  private const UPPER = '_ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  private const LOWER = '-abcdefghijklmnopqrstuvwxyz';
+
   private function __construct() {
   }
 
@@ -12,7 +15,16 @@ final readonly class TransformRequestDataHelper {
    * @return array<string|null>
    */
   public static function transformRequestHeaders(array $data): array {
-    return array_map(fn(array $values) => isset($values[0]) ? (string) $values[0] : null, $data);
+    return \array_map(fn(array $values) => isset($values[0]) ? (string) $values[0] : null, $data);
+  }
+
+  /**
+   * @param string $key
+   *
+   * @return string
+   */
+  public static function transformHeaderKey(string $key): string {
+    return \strtr($key, self::UPPER, self::LOWER);
   }
 
 }
